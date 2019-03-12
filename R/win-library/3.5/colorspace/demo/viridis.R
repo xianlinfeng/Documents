b18e60393523b293a031a922ec2e7ac98ce57c27
xianlinfeng <xianlinfeng86@gmail.com>
2019-03-12 22:05:02 +1100

    commit from Lite

--- R/win-library/3.5/colorspace/demo/viridis.R
+++
@@ -1,9 +0,0 @@
-if(requireNamespace("viridis")) {
-
-library("colorspace")
-
-specplot(viridis::viridis(9), sequential_hcl(9, "Viridis"), main = "Viridis")
-specplot(viridis::plasma(9), sequential_hcl(9, "Plasma"), main = "Plasma")
-specplot(viridis::inferno(9), sequential_hcl(9, "Inferno"), main = "Inferno")
-
-}
