b5a60eb042671d7c2073b40d07dbb268009a5d3d
xianlinfeng <xianlinfeng86@gmail.com>
2019-03-19 19:32:45 +1100

    from manjaro 19/3/2019

--- scip/project01/cmain.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Hellow world!\n");
-    
-    return 0;
-}
---
+++ scip/repair/build/CMakeFiles/repair.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/arthur/Documents/scip/repair/src/cmain.c" "/home/arthur/Documents/scip/repair/build/CMakeFiles/repair.dir/src/cmain.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/arthur/opt/scipoptsuite-6.0.1/scip/src"
+  "/home/arthur/opt/scipoptsuite-6.0.1/scip/build"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
